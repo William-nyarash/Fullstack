c016f0c1331657e9979387e5cdebf54981ea7f0a
william-Nyarash <owetywilliamnyaranga@gmail.com>
2025-04-08 22:43:47 +0300

    working with a bundler (webpack)

---
+++ webpack/src/index.js
@@ -0,0 +1,7 @@
+import App from './app'
+
+const hello = name => {
+console.log(`hello ${name}`)
+}
+
+App()
---
+++ webpack/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+const config = () => {
+
+	return {
+	 entry:'./src/index.js',
+	 output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'main.js'
+		}
+	 }
+	}
+
+module.exports = config
+
